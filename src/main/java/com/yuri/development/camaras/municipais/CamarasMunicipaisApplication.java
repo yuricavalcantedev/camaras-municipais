45d2fbc8c86a804003611348b38d0327c41de845
Yuri Cavalcante <yuricavalcante17@gmail.com>
2022-10-26 14:14:57 +0100

    Initial Commit

---
+++ src/main/java/com/yuri/development/camaras/municipais/CamarasMunicipaisApplication.java
@@ -0,0 +1,13 @@
+package com.yuri.development.camaras.municipais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamarasMunicipaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamarasMunicipaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yuri/development/camaras/municipais/CamarasMunicipaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuri.development.camaras.municipais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamarasMunicipaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
